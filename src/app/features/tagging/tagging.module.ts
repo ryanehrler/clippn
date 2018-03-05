a3be563e5faa373cd0b81277c9f210b4101a1156
Starknight2 <bob.mergner@gmail.com>
2018-03-05 00:21:50 -0500

    Tagging updates
    And/or logic seems to be working right with no bugs now. Also added
    autocomplete on the tags!

--- src/app/features/tagging/tagging.module.ts
+++ src/app/features/tagging/tagging.module.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../material/material.module';
 import { SharedModule } from '../../shared/shared.module';
 import { TaggingRoutingModule } from './tagging-routing.module';
 import { TaggingComponent } from './tagging/tagging.component';
 
 @NgModule({
-  imports: [CommonModule, TaggingRoutingModule, MaterialModule, SharedModule],
+  imports: [
+    CommonModule,
+    TaggingRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedModule
+  ],
   declarations: [TaggingComponent]
 })
 export class TaggingModule {}
--- src/app/features/tagging/tagging/tagging.component.ts
+++ src/app/features/tagging/tagging/tagging.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators/map';
+import { startWith } from 'rxjs/operators/startWith';
 import { Clip, Poi } from '../../../core/services/clip/index';
 
 @Component({
@@ -308,12 +312,26 @@ export class TaggingComponent implements OnInit {
   filterArray: any[] = new Array();
   filterMethod = 'or';
   foundTags: any[] = new Array();
+  tagArray: any[] = new Array();
+  filteredTagArray: Observable<string[]>;
+
+  myTagControl = new FormControl();
 
   constructor() {}
 
   ngOnInit() {
     this.clips = this.clips.filter(x => x.pois != null);
     this.clips = this.clips.filter(x => x.pois.length > 0);
+    this.filteredTagArray = this.myTagControl.valueChanges.pipe(
+      startWith(''),
+      map(val => this.filterAutocomplete(val))
+    );
+  }
+
+  filterAutocomplete(val: string): string[] {
+    return this.tagArray.filter(
+      option => option.toLowerCase().indexOf(val.toLowerCase()) === 0
+    );
   }
 
   applyFilter(filterInput: string) {
@@ -330,68 +348,90 @@ export class TaggingComponent implements OnInit {
       poi.tags.forEach(tag => {
         this.filterTags(tag.value.toLowerCase());
       });
+    });
 
-      // or version
-      if (this.filterMethod == 'or') {
-        if (
-          poi.tags.some(r => this.foundTags.indexOf(r.value.toLowerCase()) > -1)
-        ) {
-          matched = true;
-          return true;
-        }
+    clip.pois.some(poi => {
+      if (this.havePoiMatch(poi)) {
+        matched = true;
+        return true;
+      } else {
+        matched = false;
+        return false;
       }
-      // and version
-      if (this.filterMethod == 'and') {
-        if (
-          this.foundTags.every(
-            r => poi.tags.findIndex(x => x.value.toLowerCase() == r) > -1
-          )
-        ) {
-          matched = true;
-          return true;
-        }
-      }
-
-      // i think this could be used for an 'exact' filter version
-      // if (poi.tags.every(r => this.foundTags.indexOf(r.value) > -1)) {
-      //   matched = true;
-      //   return true;
-      // }
     });
     return matched;
   }
 
   // these probably need better function names -- and this one filters the poi row out
   havePoiMatch(poi: Poi) {
-    let matched = false;
-
     // or version
     if (this.filterMethod == 'or') {
       if (
         poi.tags.some(r => this.foundTags.indexOf(r.value.toLowerCase()) > -1)
       ) {
-        matched = true;
+        return true;
+      } else {
+        return false;
       }
     }
 
-    // and version
+    // and version list of sniper tags list of 'he' tags and then at least one match from each tag to trigger true
     if (this.filterMethod == 'and') {
-      if (
-        this.foundTags.every(
-          r => poi.tags.findIndex(x => x.value.toLowerCase() == r) > -1
-        )
-      ) {
-        matched = true;
+      if (this.filterArray.length > 0) {
+        // loop through poi tags if tag hits index of all in array then we're good. filter array and found tags find one of each
+        if (
+          this.filterArray.every(
+            r =>
+              poi.tags.findIndex(x => x.value.toLowerCase().indexOf(r) > -1) >
+              -1
+          )
+        ) {
+          if (this.filterValue) {
+            if (
+              poi.tags.findIndex(
+                x => x.value.toLowerCase().indexOf(this.filterValue) > -1
+              ) > -1
+            ) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+          return true;
+        }
+        return false;
+      }
+      if (this.filterValue) {
+        if (
+          poi.tags.findIndex(
+            x => x.value.toLowerCase().indexOf(this.filterValue) > -1
+          ) > -1
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
         return true;
       }
     }
 
-    return matched;
+    // i think this could be used for an 'exact' filter version
+    // if (poi.tags.every(r => this.foundTags.indexOf(r.value) > -1)) {
+    //   matched = true;
+    //   return true;
+    // }
+  }
+  tagSelected(tagOption: any) {
+    this.filterValue = tagOption.option.value;
   }
 
   filterTags(tag: string) {
     if (tag) {
       tag = tag.toLowerCase();
+      if (this.tagArray.indexOf(tag) < 0) {
+        this.tagArray.push(tag);
+      }
       if (this.filterArray.length > 0) {
         if (this.filterArray.some(x => tag.indexOf(x) > -1)) {
           if (this.foundTags.indexOf(tag) < 0) {
@@ -399,7 +439,7 @@ export class TaggingComponent implements OnInit {
           }
           return true;
         }
-        if (this.filterValue != '' && tag.indexOf(this.filterValue) > -1) {
+        if (this.filterValue && tag.indexOf(this.filterValue) > -1) {
           if (this.foundTags.indexOf(tag) < 0) {
             this.foundTags.push(tag);
           }
