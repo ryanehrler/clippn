330bdfb3637b43a277fc771874c33e49580d67ed
Ryan Ehrler <ryanehrler@gmail.com>
2018-03-05 21:14:53 -0500

    -Added Fortnite support --Right now detecting eliminations --In the future will need to support knockdowns as well

--- src/app/core/services/game-analyzer/fortnite-analyzer.service.ts
+++ src/app/core/services/game-analyzer/fortnite-analyzer.service.ts
@@ -18,16 +18,16 @@ export class FortniteAnalyzerService extends GameAnalyzerBase
 
   totalFrameCount = 0;
   missedKillDetectionThreshold = 2;
-  durationOfKill = 1.8;
+  durationOfKill = 2.1; //71 frames
   fireThresholdSeconds = 10;
 
   analysisFPS = 5;
   analysisVideoWidth = 1920;
   analysisVideoHeight = 1080;
 
-  pixelRedValue = 154;
-  pixelGreenValue = 47;
-  pixelBlueValue = 54;
+  pixelRedValue = 221;
+  pixelGreenValue = 25;
+  pixelBlueValue = 27;
   pixelRedTolerance = 15;
   pixelGreenTolerance = 15;
   pixelBlueTolerance = 35;
@@ -37,16 +37,71 @@ export class FortniteAnalyzerService extends GameAnalyzerBase
   }
 
   hasPoi(colorArray: Uint8Array) {
-    // console.log('fortnite-has-poi');
-    return false;
+    let kill = false;
+    let index = 0;
+    while (index < colorArray.length) {
+      const r = colorArray[index];
+      const g = colorArray[index + 1];
+      const b = colorArray[index + 2];
+
+      // console.log(r, g, b);
+      if (
+        this.isAround(r, this.pixelRedValue, this.pixelRedTolerance) &&
+        this.isAround(g, this.pixelGreenValue, this.pixelGreenTolerance) &&
+        this.isAround(b, this.pixelBlueValue, this.pixelBlueTolerance)
+      ) {
+        kill = true;
+        break;
+      }
+
+      index += 4;
+    }
+
+    return kill;
   }
 
-  processVideo() {
+  processVideo(webGl: any, canvas2d: any, video: any, currentTime: any) {
     // console.log('fortnite-process-video');
-    if (this.isResolutionSet()) {
+    if (!this.isResolutionSet()) {
       return false;
     }
-    return false;
+
+    const w = this.scaledWidth;
+    const h = this.scaledHeight;
+    const xS = this.scaledXStart;
+    const yS = this.scaledYStart;
+    // console.log(w, h, xS, yS);
+
+    const typedArray = new Uint8Array(w * h * 4);
+
+    webGl.texImage2D(
+      webGl.TEXTURE_2D,
+      0,
+      webGl.RGBA,
+      webGl.RGBA,
+      webGl.UNSIGNED_BYTE,
+      video
+    );
+    webGl.readPixels(xS, yS, w, h, webGl.RGBA, webGl.UNSIGNED_BYTE, typedArray);
+
+    // Render video partial to canvas
+    const palette = canvas2d.getImageData(0, 0, w, h);
+    palette.data.set(new Uint8ClampedArray(typedArray));
+    canvas2d.putImageData(palette, 0, 0);
+
+    if (this.hasPoi(typedArray)) {
+      this.addDetection();
+      // console.log('POI Detected');
+    } else {
+      this.missedDetection();
+    }
+
+    return this.processKillDetectionCounts(
+      currentTime,
+      this.analysisFPS,
+      this.durationOfKill,
+      this.missedKillDetectionThreshold
+    );
   }
 
   reset() {
@@ -68,5 +123,13 @@ export class FortniteAnalyzerService extends GameAnalyzerBase
     this.height = this.scaledHeight;
     this.xStart = this.scaledXStart;
     this.yStart = this.scaledYStart;
+
+    // console.log(
+    //   'set-video-res',
+    //   this.width,
+    //   this.height,
+    //   this.xStart,
+    //   this.yStart
+    // );
   }
 }
