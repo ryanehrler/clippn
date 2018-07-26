dca84fb47a38387a0b543262cbe84f665977c9dd
Ryan Ehrler <ryanehrler@gmail.com>
2018-07-25 23:31:39 -0400

    Updated login module with key input and validation - Added HttpErrorInterceptor

--- src/app/features/login/login-modal/login-modal.component.ts
+++ src/app/features/login/login-modal/login-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { IUser } from '../../../shared/models/user';
+import { HttpCdKeyService } from '../../../core/services/http';
 
 @Component({
   selector: 'app-login-modal',
@@ -9,11 +10,43 @@ import { IUser } from '../../../shared/models/user';
   styleUrls: ['./login-modal.component.scss']
 })
 export class LoginModalComponent implements OnInit {
-  user = {} as IUser;
-  constructor(public auth: AuthService, public snackBar: MatSnackBar) {}
+  user: IUser;
+  key: string;
+  keyIsValid: boolean;
 
-  ngOnInit() {}
+  constructor(
+    public auth: AuthService,
+    public snackBar: MatSnackBar,
+    private httpCdKeyService: HttpCdKeyService
+  ) {}
 
+  ngOnInit() {
+    this.user = {
+      email: '',
+      uid: ''
+    };
+  }
+
+  validateKey() {
+    console.log(this.key);
+
+    // this.httpCdKeyService.validateKey(this.key).then(res => {
+    //   console.log(res);
+    // });
+
+    this.httpCdKeyService.validateKey(this.key).subscribe(res => {
+      console.log(res);
+
+      if (res.key !== undefined) {
+        this.keyIsValid = true;
+      }
+    });
+  }
+  reValidateKey() {
+    if (this.keyIsValid) {
+      this.keyIsValid = false;
+    }
+  }
   registerUser(user: IUser) {
     if (user.password === user.passwordConfirm) {
       this.auth.createUser(user);
